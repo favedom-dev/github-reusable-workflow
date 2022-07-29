fc4dd9002dfacb0baa0856716140f0cf1d4efde4
Matt Masters <mmasters@velocityz.com>
2022-07-29 15:18:12 -0400

    debug

--- scripts/auto-increment-version.sh
+++ scripts/auto-increment-version.sh
@@ -48,6 +48,7 @@ if [ -z "$NEEDS_TAG" ]; then
     echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
     # git tag $NEW_TAG
     # git push --tags
+    env
     gh release create $NEW_TAG --generate-notes
     rc=$?
     if [ ${rc} -ne 0 ] ; then
