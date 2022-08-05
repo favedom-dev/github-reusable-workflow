4434125b34e87955a01a1c60451261a55a6ae0f6
Matt Masters <mmasters@velocityz.com>
2022-08-05 15:02:59 -0400

    preview_copy_secrets.sh depends on local ./preview_secrets.txt

--- scripts/preview_copy_secrets.sh
+++ scripts/preview_copy_secrets.sh
@@ -1,57 +1,64 @@
 #!/bin/bash
 
-# GitHub Actions: ${{ github.event.number }}
-# export PR_NUM=67GHA
-# export APP_NAME=peeq-sms
-#
-# export SECRET_NAMESPACE=jx-staging
-# export SECRETS_STAGING=("rabbitmq" "peeq-users" "peeq-sms-twilio" "jx-staging-peeq-sms-pg")
-# ./preview_copy_secrets.sh "${SECRETS_STAGING[@]}"
-#
-# export SECRET_NAMESPACE=jx
-# export SECRETS_JX=("stackhawk-fan" "stackhawk-preview")
-# ./preview_copy_secrets.sh "${SECRETS_JX[@]}"
+# Format for contents of file SECRET_FILENAME is:
+#    namespace + ":" + secret names "," separated
+# Examples:
+# namespace1:secretA,secretB namespace2:secretX,secretY,secretZ
+# jx-staging:rabbitmq jx:stackhawk-fan,stackhawk-preview
+DEFAULT_SECRET_FILENAME="./preview_secrets.txt"
+# 1st arg can override the SECRET_FILENAM name
+SECRET_FILENAME="${1:-$DEFAULT_SECRET_FILENAME}"
 
-# PREVIEW_NAMESPACE=$(tr '[:upper:]' '[:lower:]' <<< ${APP_NAME}-PR-${PR_NUM})
+# check that file exists
+if [ ! -f "$SECRET_FILENAME" ]; then
+  echo "ERROR: \"${SECRET_FILENAME}\" does not exists in: $(pwd)"
+  exit 1
+fi
 
+# check that preview namespace exists
+if [[ $(kubectl get ns | awk '{print $1}' | grep ^${PREVIEW_NAMESPACE}$) != ${PREVIEW_NAMESPACE} ]]; then
+  echo "ERROR: namespace \"${PREVIEW_NAMESPACE}\" does not exist"
+  exit 1
+fi
+
+echo "-----------------:-----------------"
 echo "PREVIEW_NAMESPACE: ${PREVIEW_NAMESPACE}"
+echo "SECRET_FILENAME  : ${SECRET_FILENAME}"
+echo "-----------------:-----------------"
 
-check_vars() {
-    var_names=("$@")
-    for var_name in "${var_names[@]}"; do
-        [ -z "${!var_name}" ] && echo "$var_name is unset." && var_unset=true
-    done
-    [ -n "$var_unset" ] && exit 1
-    return 0
-}
+NAMESPACE_SECRETS=$(<"${SECRET_FILENAME}")
 
-function copy-secret() {
-    check_vars \
-    SECRET_NAMESPACE
-    # APP_NAME \
-    # PR_NUM \
+for secret_namespace in ${NAMESPACE_SECRETS[@]}; do
+  # clean array
+  unset secretList
+  # get secret_namespace
+  if [[ ${NAMESPACE_SECRETS} == *":"* ]]; then
+    # split namespace name from sub-list secrets
+    tmpNamespaceArray=(${secret_namespace//:/ })
+    secret_namespace=${tmpNamespaceArray[0]}
+    secretList=${tmpNamespaceArray[1]}
+    # make array from simple string
+    secretList=(${secretList//,/ })
+  fi
 
+  echo ""
+  echo "======:==================:================"
+  echo "START : secret namespace : ${secret_namespace}"
+  echo "======:==================:================"
 
-    local arraySecrets=("$@")
-    if [ ${#arraySecrets[@]} -lt 1 ] ; then
-      echo "Array length: ${#arraySecrets[@]}"
-      echo "Secret array invalid"
-      exit 1
-    fi
+  # get secrets
+  for secret in ${secretList[@]}; do
+    # copy secret
+    echo ""
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    echo "Copy secret..."
+    echo "Preview Namespace : ${PREVIEW_NAMESPACE}"
+    echo "Secret Namespace  : ${secret_namespace}"
+    echo "Secret            : ${secret}"
+    echo "---------------------------------------------"
+    kubectl delete secret --namespace=${PREVIEW_NAMESPACE} ${secret} --ignore-not-found
+    kubectl get secret ${secret} --namespace=${secret_namespace} -o yaml | sed 's/namespace: '${secret_namespace}'/namespace: '${PREVIEW_NAMESPACE}'/g' | kubectl create --namespace=${PREVIEW_NAMESPACE} -f -
+    echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+  done
 
-    for i in "${arraySecrets[@]}"; do
-        echo ""
-        echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-        echo "Copy secret..."
-        echo "PREVIEW_NAMESPACE : ${PREVIEW_NAMESPACE}"
-        echo "SECRET_NAMESPACE  : ${SECRET_NAMESPACE}"
-        echo "SECRET            : ${i}"
-        echo "---------------------------------------------"
-        kubectl delete secret --namespace=${PREVIEW_NAMESPACE} ${i} --ignore-not-found
-        kubectl get secret ${i} --namespace=${SECRET_NAMESPACE} -o yaml | sed 's/namespace: '${SECRET_NAMESPACE}'/namespace: '${PREVIEW_NAMESPACE}'/g' | kubectl create --namespace=${PREVIEW_NAMESPACE} -f -
-        echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    done
-}
-
-## MAIN
-copy-secret "$@"
+done
