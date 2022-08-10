77981fedd315d811b45d10b7d35fb9208d46a33b
Matt Masters <mmasters@velocityz.com>
2022-08-10 09:55:10 -0400

    setup-stackhawk.sh

---
+++ stackhawk/setup-stackhawk.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# NOTE: requires the following env variables which are used in stackhawk-tmpl.yml
+# APP_NAME
+# PR_HOST
+# PQ_API_PATH
+# KEYCLOAK_AUTH
+# TEST_USERNAME
+# TEST_PASSWORD
+
+chmod 777 *
+
+# Get token and save value to variable
+echo "Get AUTH_TOKEN"
+export AUTH_TOKEN=$(curl -X POST "${KEYCLOAK_AUTH}" \
+ -H "Content-Type: application/x-www-form-urlencoded" \
+ -d "username=${TEST_USERNAME}" \
+ -d "password=${TEST_PASSWORD}" \
+ -d 'grant_type=password' \
+ -d "client_id=peeq-query" | jq -r '.access_token')
+
+# update stackhawk.yml with variables
+envsubst < ./stackhawk-tmpl.yml > ./stackhawk.yml
