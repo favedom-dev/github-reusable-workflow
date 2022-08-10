7a4e28b7a2513ff4cc8b0d0d7b287c150b6c9733
Matt Masters <mmasters@velocityz.com>
2022-08-10 16:33:33 -0400

    overrite files

--- scripts/setup_ci.sh
+++ scripts/setup_ci.sh
@@ -2,13 +2,13 @@
 
 get_ci_yaml() {
   mkdir -p .github/workflows
-  wget https://raw.githubusercontent.com/favedom-dev/github-reusable-workflow/master/templates/${CI_DIR}/ci.yaml -P .github/workflows/
-  wget https://raw.githubusercontent.com/favedom-dev/github-reusable-workflow/master/templates/preview-cleanup.yaml -P .github/workflows/
+  wget -O .github/workflows/ci.yaml https://raw.githubusercontent.com/favedom-dev/github-reusable-workflow/master/templates/${CI_DIR}/ci.yaml
+  wget -O .github/workflows/preview-cleanup.yaml https://raw.githubusercontent.com/favedom-dev/github-reusable-workflow/master/templates/preview-cleanup.yaml
 }
 
 get_secrets_template() {
   mkdir -p scripts
-  wget https://raw.githubusercontent.com/favedom-dev/github-reusable-workflow/master/scripts/preview_secrets.txt -P scripts/
+  wget -O scripts/preview_secrets.txt https://raw.githubusercontent.com/favedom-dev/github-reusable-workflow/master/scripts/preview_secrets.txt
 }
 
 arg_ci_dir() {
