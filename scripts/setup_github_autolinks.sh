ab15343f36162a76d9d0bb8aa50b806afea7987c
Matt Masters <mmasters@velocityz.com>
2022-08-16 10:00:13 -0400

    feat: setup github autolinks

---
+++ scripts/setup_github_autolinks.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+ORG_NAME="favedom-dev"
+
+setup_repo_name() {
+  export REPO_NAME=$(basename `git rev-parse --show-toplevel`)
+  display_vars
+}
+
+display_vars() {
+  echo ""
+  echo "-----------:-------------------"
+  echo "ORG_NAME   : ${ORG_NAME}"
+  echo "REPO_NAME   : ${REPO_NAME}"
+  echo "-----------:-------------------"
+}
+
+set_autolink() {
+  echo ""
+  echo "Setup: PQ-"
+  gh api \
+  --method POST \
+  -H "Accept: application/vnd.github+json" \
+  /repos/${ORG}/${REPO_NAME}/autolinks \
+  -f key_prefix='PQ-'
+  -f url_template='https://velocityz.atlassian.net/browse/PQ-<num>'
+
+  echo ""
+  echo "Setup: PQK-"
+  gh api \
+  --method POST \
+  -H "Accept: application/vnd.github+json" \
+  /repos/${ORG}/${REPO_NAME}/autolinks \
+  -f key_prefix='PQK-'
+  -f url_template='https://velocityz.atlassian.net/browse/PQK-<num>'
+}
+
+cleanup() {
+  # rm $0
+  echo "TODO: cleanup"
+}
+
+## MAIN
+setup_repo_name
+set_autolink
+cleanup
