ecab2848297ccfbfbc04b85e7cfce4e462b1df1a
mmastersvz <mmasters@velocityz.com>
2024-05-21 12:03:35 -0400

    feat: improved handling for the image

--- scripts/update_tag.sh
+++ scripts/update_tag.sh
@@ -1,41 +1,76 @@
-#/bin/bash
+#!/usr/bin/env bash
 
-# YAML_FILE="helm/values/identityx-v17.yaml"
+# TEST VALUES
+# NAME="identityx-24"
 # VERSION="9.9.999"
-# NAME="peeq-keycloak"
-# SED_GCR_REPO="gcr.io\/favedom-dev\/"
-# SED_GAR_REPO="us-central1-docker.pkg.dev\/favedom-dev\/peeq-docker\/"
 
-ELEMENT_NAME_1="repository"
-ELEMENT_NAME_2="tag"
+YAML_FILE="${YAML_FILE:-"values.yaml"}"
+REGISTRY="${REGISTRY:-"us-central1-docker.pkg.dev"}"
+REPOSITORY="${REPOSITORY:-"favedom-dev/docker"}"
+ROOT_ELEMENT="${ROOT_ELEMENT:-${NAME}}"
+SUB_ELEMENT="${SUB_ELEMENT:-"image"}"
+BASE_ELEMENT="${BASE_ELEMENT:-${ROOT_ELEMENT}.${SUB_ELEMENT}}"
 
-echo "--------------:--------------"
-echo "NAME          : ${NAME}"
-echo "VERSION       : ${VERSION}"
-echo "YAML_FILE     : ${YAML_FILE}"
-echo "SED_GCR_REPO  : ${SED_GCR_REPO}"
-echo "SED_GAR_REPO  : ${SED_GAR_REPO}"
-echo "ELEMENT_NAME_1: ${ELEMENT_NAME_1}"
-echo "ELEMENT_NAME_2: ${ELEMENT_NAME_2}"
-echo "--------------:--------------"
+echo "---------------:--------------"
+echo "YAML_FILE      : ${YAML_FILE}"
+echo "NAME           : ${NAME}"
+echo "VERSION        : ${VERSION}"
+echo "REGISTRY       : ${REGISTRY}"
+echo "REPOSITORY     : ${REPOSITORY}"
+echo "ROOT_ELEMENT   : ${ROOT_ELEMENT}"
+echo "SUB_ELEMENT    : ${SUB_ELEMENT}"
+echo "BASE_ELEMENT  -: ${BASE_ELEMENT}"
+echo "---------------:--------------"
 
+YAML_REGISTRY=$(cat ./${YAML_FILE} | yq eval '.'"${BASE_ELEMENT}"'.registry')
+YAML_REPOSITORY=$(cat ./${YAML_FILE} | yq eval '.'"${BASE_ELEMENT}"'.repository')
+YAML_TAG=$(cat ./${YAML_FILE} | yq eval '.'"${BASE_ELEMENT}"'.tag')
 
-echo "Using:"
-if [[ $(grep ${SED_GAR_REPO}${NAME} ${YAML_FILE}) ]]; then
-  echo "SED_GAR_REPO  : ${SED_GAR_REPO}"
-  REPO_STR=${SED_GAR_REPO}
-elif [[ $(grep ${SED_GCR_REPO}${NAME} ${YAML_FILE}) ]]; then
-  echo "SED_GCR_REPO  : ${SED_GCR_REPO}"
-  REPO_STR=${SED_GCR_REPO}
+echo "YAML_REGISTRY  : ${YAML_REGISTRY}"
+echo "YAML_REPOSITORY: ${YAML_REPOSITORY}"
+echo "YAML_TAG       : ${YAML_TAG}"
+echo "---------------:--------------"
+echo ""
+
+update_yaml() {
+  echo "Updating ${YAML_ELEMENT}: ${NEW_VALUE}"
+  yq eval -i '.'"${YAML_ELEMENT}"' = "'"${NEW_VALUE}"'"' "${YAML_FILE}"
+}
+
+update_registry() {
+  YAML_ELEMENT="${BASE_ELEMENT}.registry"
+  NEW_VALUE="${NEW_REGISTRY}"
+  update_yaml
+}
+
+update_repository() {
+  YAML_ELEMENT="${BASE_ELEMENT}.repository"
+  NEW_VALUE="${NEW_REPOSITORY}"
+  update_yaml
+}
+
+update_tag() {
+  YAML_ELEMENT="${BASE_ELEMENT}.tag"
+  NEW_VALUE="${NEW_TAG}"
+  update_yaml
+}
+
+NEW_REGISTRY="${REGISTRY}"
+NEW_REPOSITORY="${REPOSITORY}/${NAME}"
+NEW_TAG="${VERSION}"
+
+if [ "${YAML_TAG}" != "null" ]; then
+  update_tag
 else
-  echo "ERROR: not found image.${ELEMENT_NAME_1}: ${NAME}"
-  exit 1
+  NEW_REPOSITORY="${NEW_REPOSITORY}:${NEW_TAG}"
 fi
 
-REPO_STR=${REPO_STR}${NAME}
+if [ "${YAML_REGISTRY}" != "null" ]; then
+  update_registry
+else
+  NEW_REPOSITORY="${NEW_REGISTRY}/${NEW_REPOSITORY}"
+fi
 
-echo "--------------:--------------"
-echo "REPO_STR      : ${REPO_STR}"
-echo "--------------:--------------"
-
-sed -i -e '/'${ELEMENT_NAME_1}': '${REPO_STR}'$/{n' -e 's/'${ELEMENT_NAME_2}': [0-9,.]*/'${ELEMENT_NAME_2}': '${VERSION}'/' -e '}' ${YAML_FILE}
+if [ "${YAML_REPOSITORY}" != "null" ]; then
+  update_repository
+fi
