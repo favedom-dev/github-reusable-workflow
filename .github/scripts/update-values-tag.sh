ef2006042afc175e6907254794402388a3347317
mmastersvz <mmasters@velocityz.com>
2024-06-28 17:37:27 -0400

    feat: new repo placeholder need to test

--- .github/scripts/update-values-tag.sh
+++ .github/scripts/update-values-tag.sh
@@ -10,6 +10,7 @@ fi
 CHART_DIR=$1
 TARGET_DEP=$2
 NEW_IMAGE_TAG=$3
+NEW_REPO=${4:-"us-central1-docker.pkg.dev/favedom-dev/docker/${CHART_DIR}"}
 # Build variables
 REQUIREMENTS_YAML=${CHART_DIR}/requirements.yaml
 CHART_YAML=${CHART_DIR}/Chart.yaml
@@ -24,6 +25,7 @@ echo ""
 echo "CHART_DIR         : ${CHART_DIR}"
 echo "TARGET_DEP        : ${TARGET_DEP}"
 echo "NEW_IMAGE_TAG     : ${NEW_IMAGE_TAG}"
+echo "NEW_REPO          : ${NEW_REPO}"
 echo ""
 echo "DEPENDENCIES_YAML : ${DEPENDENCIES_YAML}"
 echo "REQUIREMENTS_YAML : ${REQUIREMENTS_YAML}"
@@ -75,6 +77,17 @@ ESCAPED_DEP=$(echo "${DEPENDENCIES}" | sed 's/\./\\./g')
 echo "ESCAPED_DEP       : ${ESCAPED_DEP}"
 
 # Use sed to update the image tag associated with the specified dependency
-sed -i.bak '/'"${ESCAPED_DEP}"':/,/^[^ ]/ s/\(tag:\s*\).*/\1'"${NEW_IMAGE_TAG}"'/' "${VALUES_YAML}"
+sed -i '/'"${ESCAPED_DEP}"':/,/^[^ ]/ s/\(tag:\s*\).*/\1'"${NEW_IMAGE_TAG}"'/' "${VALUES_YAML}"
 echo ""
 echo "Updated image tag in ${VALUES_YAML} to ${NEW_IMAGE_TAG} for dependency: ${TARGET_DEP}"
+echo ""
+
+echo "TODO: update repository... to: ${NEW_REPO}"
+# sed -i '/'"${ESCAPED_DEP}"':/,/^[^ ]/ s/\(repository:\s*\).*/\1'"${NEW_REPO}"'/' "${VALUES_YAML}"
+# echo ""
+# echo "Updated image repository in ${VALUES_YAML} to ${NEW_REPO} for dependency: ${TARGET_DEP}"
+
+echo ""
+echo "${VALUES_YAML}:"
+yq "${VALUES_YAML}"
+echo ""
