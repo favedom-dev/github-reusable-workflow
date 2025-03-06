f6e21f5222fe03cd61aa55cb4b0d6bc9d88346c8
mmastersvz <mmasters@velocityz.com>
2025-03-06 14:37:22 -0500

    feat: GitHub vars

--- .github/scripts/docker-link.sh
+++ .github/scripts/docker-link.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export GH_TOKEN="${GH_TOKEN}"
+
 echo "-----------------------------------------------------"
 echo "NAME             : ${NAME}"
 echo "VERSION          : ${VERSION}"
@@ -8,7 +10,14 @@ echo "GITHUB_EVENT_NAME: ${GITHUB_EVENT_NAME}"
 echo "====================================================="
 
 echo "${DOCKER_URL}"
-if [ "${GITHUB_EVENT_NAME}" = "pull_request" ]; then
-  gh pr comment ${GITHUB_EVENT_NAME} \
+if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
+  # Extract PR number from GitHub event payload
+  PR_NUMBER=$(jq --raw-output .number "${GITHUB_EVENT_PATH}")
+  echo "PR_NUMBER : ${PR_NUMBER}"
+
+  gh pr comment ${PR_NUMBER} \
+  --repo "${GITHUB_REPOSITORY}" \
   --body "🐳 Container: [${NAME} ${VERSION}](${DOCKER_URL})"
+else
+  echo "ℹ️ Not a pull request event, skipping comment."
 fi
