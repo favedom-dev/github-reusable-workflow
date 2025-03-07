aed4852c737613c2bcd18fc07ad653fd6fe7dea7
mmastersvz <mmasters@velocityz.com>
2025-03-07 09:48:40 -0500

    feat: maven settings.xml update

--- .github/scripts/mvn-settings.xml.sh
+++ .github/scripts/mvn-settings.xml.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
 # Input and output files
-INPUT_FILE="./settings.xml"
-OUTPUT_FILE="~/.m2/settings.xml"
+INPUT_FILE=${INPUT_FILE:-../settings.xml}
+OUTPUT_FILE=${OUTPUT_FILE:-${HOME}/.m2/settings.xml}
 
 echo "-----------------------------------------------------"
 echo "INPUT_FILE : ${INPUT_FILE}"
 echo "OUTPUT_FILE: ${OUTPUT_FILE}"
 echo "====================================================="
 
-# envsubst < ./settings.xml > ~/.m2/settings.xml
-mkdir -p ~/.m2
+if command -v envsubst >/dev/null 2>&1; then
+  echo "envsubst is installed"
+  envsubst < "${INPUT_FILE}" > "${OUTPUT_FILE}"
+else
+  echo "envsubst is not installed..."
+  mkdir -p ~/.m2
 
-# Read the entire content of the file into a variable
-file_content=$(< "${INPUT_FILE}")
+  # Read the entire content of the file into a variable
+  file_content=$(< "${INPUT_FILE}")
 
-# Replace variables dynamically in the file content
-# Escape quotes and use eval for variable expansion
-eval "echo \"$(echo "$file_content" | sed 's/"/\\"/g')\"" > "${OUTPUT_FILE}"
+  # Replace variables dynamically in the file content
+  # Escape quotes and use eval for variable expansion
+  eval "echo \"$(echo "${file_content}" | sed 's/"/\\"/g')\"" > "${OUTPUT_FILE}"
+fi
 
-echo "Variables replaced and saved to ${OUTPUT_FILE}"
+echo "Variables replaced and saved to: ${OUTPUT_FILE}"
 
 echo "====="
 cat ${OUTPUT_FILE}
